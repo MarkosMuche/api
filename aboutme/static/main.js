a96f19784f51eb6415328be9752043a9a5b37765
Abi <Mesfin>
2022-02-25 11:53:06 +0300

    modified by mesfin g.

--- aboutme/static/main.js
+++ aboutme/static/main.js
@@ -3,4 +3,53 @@ collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
-);
+); 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // Modal Image Gallery
+          function onClick(element) {
+              document.getElementById("img01").src = element.src;
+              document.getElementById("modal01").style.display = "block";
+              var captionText = document.getElementById("caption");
+              captionText.innerHTML = element.alt;
+            }
+            
+            
+            // Toggle between showing and hiding the sidebar when clicking the menu icon
+            var mySidebar = document.getElementById("mySidebar");
+            
+            function w3_open() {
+            if (mySidebar.style.display === 'block') {
+                mySidebar.style.display = 'none';
+            } else {
+                mySidebar.style.display = 'block';
+            }
+        }
+        
+        // Close the sidebar with the close button
+        function w3_close() {
+            mySidebar.style.display = "none";
+        }
+          function myFunction() {
+              var x = document.getElementById("Demo");
+              if (x.className.indexOf("w3-show") == -1) {
+                  x.className += " w3-show";
+                } else { 
+                    x.className = x.className.replace(" w3-show", "");
+                }
+            }
+            function mmyFunction() {
+                var x = document.getElementById("DDemo");
+                if (x.className.indexOf("w3-show") == -1) {
+                    x.className += " w3-show";
+                } else { 
+                    x.className = x.className.replace(" w3-show", "");
+                }
+            }
